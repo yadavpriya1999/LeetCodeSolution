9b658e003daea200afd1a641963917626429b912
PRIYA YADAV <86567092+yadavpriya1999@users.noreply.github.com>
2023-08-08 00:16:37 +0530

    Time: 4 ms (26.61%), Space: 8.2 MB (37.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int s=0;
+        int e=nums.size()-1;
+        int m=0;
+        while(m<=e){
+            if(nums[m]==0){
+                swap(nums[s],nums[m]);
+                s++,m++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else{
+                swap(nums[m],nums[e]);
+                e--;
+            }
+        }
     }
 };
